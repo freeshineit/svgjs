52d16e052e98c35d0e43ca8406d9e1df17445ccb
ShineShao <xiaoshaoqq@gmail.com>
2023-02-25 00:00:55 +0800

    svg

--- app/index.ts
+++ app/index.ts
@@ -2,9 +2,18 @@ import "./style.scss";
 
 import("../wasm/pkg").then((module) => {
   function init() {
+    const svgDom = document.getElementById("svg");
     const circle1 = document.getElementById("circle1");
     circle1.setAttribute("fill", "red");
-    circle1.setAttribute("cx", "200");
+    // circle1.setAttribute("cx", "200");
+
+    circle1.addEventListener(
+      "click",
+      () => {
+        circle1.setAttribute("r", 20 + +circle1.getAttribute("r") + "");
+      },
+      false
+    );
   }
 
   init();
