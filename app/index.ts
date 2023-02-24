6279b0f38376b9a296e98c519bc660c3cd9accf7
ShineShao <xiaoshaoqq@gmail.com>
2023-02-25 01:18:26 +0800

    feat: svg.js

--- app/index.ts
+++ app/index.ts
@@ -1,20 +1,34 @@
 import "./style.scss";
 
-import("../wasm/pkg").then((module) => {
-  function init() {
-    const svgDom = document.getElementById("svg");
-    const circle1 = document.getElementById("circle1");
-    circle1.setAttribute("fill", "red");
-    // circle1.setAttribute("cx", "200");
+import { SVG } from "@svgdotjs/svg.js";
 
-    circle1.addEventListener(
-      "click",
-      () => {
-        circle1.setAttribute("r", 20 + +circle1.getAttribute("r") + "");
-      },
-      false
-    );
-  }
+function start() {
+  const draw = SVG().addTo("body").size(300, 300);
 
-  init();
-});
+  draw.rect(100, 100).fill("red").attr({
+    x: 100,
+    y: 100
+  });
+}
+
+start();
+
+// import("../wasm/pkg").then((module) => {
+// import Svg from '../node_modules/@svgdotjs/svg.js/src/elements/Svg';
+// function init() {
+//   //   const svgDom = document.getElementById("svg");
+//   //   const circle1 = document.getElementById("circle1");
+//   //   circle1.setAttribute("fill", "red");
+//   //   // circle1.setAttribute("cx", "200");
+
+//   //   circle1.addEventListener(
+//   //     "click",
+//   //     () => {
+//   //       circle1.setAttribute("r", 20 + +circle1.getAttribute("r") + "");
+//   //     },
+//   //     false
+//   //   );
+//   // }
+
+//   // init();
+// });
