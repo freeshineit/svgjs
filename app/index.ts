40835e0b3ce27bb5e4680b5e6b3ac7e90255f875
ShineShao <xiaoshaoqq@gmail.com>
2023-02-26 22:09:08 +0800

    remove wasm

--- app/index.ts
+++ app/index.ts
@@ -12,23 +12,3 @@ function start() {
 }
 
 start();
-
-// import("../wasm/pkg").then((module) => {
-// import Svg from '../node_modules/@svgdotjs/svg.js/src/elements/Svg';
-// function init() {
-//   //   const svgDom = document.getElementById("svg");
-//   //   const circle1 = document.getElementById("circle1");
-//   //   circle1.setAttribute("fill", "red");
-//   //   // circle1.setAttribute("cx", "200");
-
-//   //   circle1.addEventListener(
-//   //     "click",
-//   //     () => {
-//   //       circle1.setAttribute("r", 20 + +circle1.getAttribute("r") + "");
-//   //     },
-//   //     false
-//   //   );
-//   // }
-
-//   // init();
-// });
